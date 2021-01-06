9d56794a2e1dfe9887b4732764e2b691fbb912b6
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-01-06 11:00:25 +0300

    Basics

--- Basics/test48.js
+++ Basics/test48.js
@@ -1,10 +1,6 @@
 function isLess(a, b) {
     // Only change code below this line
-    if (a < b) {
-        return true;
-    } else {
-        return false;
-    }
+    return a < b;
     // Only change code above this line
 }
 
---
+++ Basics/test51.js
@@ -0,0 +1,9 @@
+var myDog = {
+  // Only change code below this line
+  name: "Doggy",
+  legs: 4,
+  tails: 1,
+  friends: ["Me", "Myself", "I"],
+
+  // Only change code above this line
+};
