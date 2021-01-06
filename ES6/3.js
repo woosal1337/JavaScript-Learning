09f2750b5d2a9b9761ad7c95683cbc316c8d09c4
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-01-06 18:34:49 +0300

    ES6

---
+++ ES6/3.js
@@ -0,0 +1,17 @@
+function printManyTimes(str) {
+
+    // Only change code below this line
+
+
+
+    let sentence = str + " is cool!";
+    for (let i = 0; i < str.length; i+=2) {
+        const SENTENCE = sentence;
+        console.log(SENTENCE);
+    }
+
+
+    // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
