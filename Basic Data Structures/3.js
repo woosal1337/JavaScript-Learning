0f569eb9aa4ea046a332596418018a01802139b4
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-01-11 19:29:25 +0300

    Basic Data Structures

---
+++ Basic Data Structures/3.js
@@ -0,0 +1,10 @@
+function popShift(arr) {
+    let popped; // Change this line
+    let shifted; // Change this line
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+  // pop() = Deleting the last element from the array
+  // shift() = Deleting the first element from the array
